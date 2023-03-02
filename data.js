6e60f7914182761bb0edff4ade1cb883aa8b13de
Victor <nastoyashchiy@gmail.com>
2023-03-03 01:23:59 +0200

    start

---
+++ data.js
@@ -0,0 +1,57 @@
+export default [
+    {
+        author: 'Jeep',
+        content: 'https://www.jeep.com/',
+        image: './images/pexels-aaron-curtis-119435.jpg'
+    },
+    {
+        author: 'Dodge',
+        content: 'https://www.dodge.com/',
+        image: './images/pexels-garvin-st-villier-3311574.jpg'
+    },
+    {
+        author: 'Lada',
+        content: 'https://en.wikipedia.org/wiki/Lada',
+        image: './images/pexels-johnmark-smith-253096.jpg'
+    },
+    {
+        author: 'Mercedes',
+        content: 'https://www.mercedes-benz.com/en/',
+        image: './images/pexels-mike-b-112460.jpg'
+    },
+    {
+        author: 'BMW',
+        content: 'https://www.bmw.com/en/index.html',
+        image: './images/pexels-mike-b-170811.jpg'
+    },
+    {
+        author: 'Audi',
+        content: 'https://www.audi.com/en.html',
+        image: './images/pexels-mike-b-1035108.jpg'
+    },
+    {
+        author: 'Mercedes',
+        content: 'https://www.mercedes-benz.com/en/',
+        image: './images/pexels-mike-b-2365572.jpg'
+    },
+    {
+        author: 'Mini Cooper',
+        content: 'https://www.mini.com/en_MS/home.html',
+        image: './images/pexels-pixabay-35967.jpg'
+    },
+    {
+        author: 'Hyundai',
+        content: 'https://www.hyundai.com/worldwide/en/',
+        image: './images/pexels-s-von-hoerst-2920064.jpg'
+    },
+    {
+        author: 'BMW',
+        content: 'https://www.bmw.com/en/index.html',
+        image: './images/pexels-torsten-dettlaff-70912.jpg'
+    },
+    {
+        author: 'BMW',
+        content: 'https://www.bmw.com/en/index.html',
+        image: './images/pexels-trace-constant-707046.jpg'
+    }
+]
